212969c1b9a8c0676ae68c4fac7d6d84564d7b24
Aline Soares <aliinanda@example.com>
2024-10-19 21:55:14 -0300

    criando repositorio

--- aed1/atv1.c
+++ aed1/atv1.c
@@ -4,15 +4,31 @@ int main(){
 
     int n1, n2, n3;
 
-    printf("Digite 3 numeros: ");
+    printf("Digite 3 numeros: \n");
     scanf("%d %d %d", &n1, &n2,&n3);
 
-    if(n1 > n2 && n1 > n3){
-        printf("%d é o maior",n1);
-        if(n2 > n3 && n2 > n1){
-            printf("%d é o menor",n2);
+    if(n1 >= n2 && n1 >= n3){
+        printf("%d e o maior\n",n1);
+        if(n2 <= n3){
+            printf("%d e o menor\n",n2);
         }else{
-            printf("%d é o menor",n3);
+            printf("%d e o menor\n",n3);
+        }
+    }
+    if(n2 > n1 && n2 >= n3){
+        printf("%d e o maior\n",n2);
+        if(n1 <= n3){
+            printf("%d e o menor\n",n1);
+        }else{
+            printf("%d e o menor\n",n3);
+        }
+    }
+    if(n3 > n1 && n3 > n2){
+        printf("%d e o maior\n",n3);
+        if(n1 <= n2){
+            printf("%d e o menor\n",n1);
+        }else{
+            printf("%d e o menor\n",n2);
         }
     }
     
