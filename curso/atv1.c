1dd4aa63e36b6e66000ca8d9f4b0093c300d7ad2
Aline Soares <aliinanda@example.com>
2024-11-06 16:14:35 -0300

    add

---
+++ curso/atv1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int n1, n2, n3, soma;
+
+    printf("Digite 3 valores: \n");
+    scanf("%d %d %d", &n1, &n2, &n3);
+
+    soma = n1 + n2 + n3;
+
+    printf("A soma é: %d", soma);
+}
